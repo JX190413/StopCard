25feddd8a4cea0620edd671ba2605495cc52dd90
JX190413 <1027324602@qq.com>
2019-12-16 18:13:33 +0800

    Initial commit

---
+++ src/test/java/com/cykj/stopcard/StopcardApplicationTests.java
@@ -0,0 +1,15 @@
+package com.cykj.stopcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StopcardApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
